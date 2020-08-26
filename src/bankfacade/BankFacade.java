9daec0ce2c1d7f80846fb4c5a321a8f85038bfcb
jirapat094 <60957643+jirapat42@users.noreply.github.com>
2020-08-26 16:45:45 +0700

    jitapat

--- src/bankfacade/BankFacade.java
+++ src/bankfacade/BankFacade.java
@@ -7,6 +7,7 @@ package bankfacade;
 
 import bankcustomers.BankAccount;
 import bankcustomers.BankCustomer;
+import bankcustomers.MyFacade;
 import gencustaccount.AccountIf;
 import gencustaccount.CustomerIf;
 import java.util.ArrayList;
@@ -16,32 +17,47 @@ import java.util.ArrayList;
  * @author sarun
  */
 public class BankFacade {
-
+    
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       ArrayList<AccountIf> cust1Accounts = new ArrayList();
-       AccountIf acc = new BankAccount(12345);
-       acc.deposit(500);
-       cust1Accounts.add(acc);
-       acc = new BankAccount(12346);
-       acc.deposit(1000);
-       cust1Accounts.add(acc);
+      
+       
+      ArrayList<AccountIf> cust1Accounts = new ArrayList();
+       AccountIf acc = new BankAccount(11111);
+       cust1Accounts.add(acc); //acc1ofcus1
+       acc = new BankAccount(11112);
+       cust1Accounts.add(acc); //acc2ofcus1
        CustomerIf cust1 = new BankCustomer("John", cust1Accounts);
-       System.out.println("Customer and account information");
-       System.out.println("Name = " + cust1.getCustomerName());
-       System.out.println("Has " + cust1.getNumAccounts() + " accounts");
-       acc = cust1.getAccount(12345);
-       System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
-       acc = cust1.getAccount(12346);
-       System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
-       acc.deposit(1000);
-       System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
-       ArrayList<AccountIf> accounts = cust1.getllAccounts();
-       for(AccountIf account : accounts) {
-           System.out.println("Account number " + account.getAccountNumber() + " has " + account.getBalance());
-       }
+       
+       
+       ArrayList<AccountIf> cust2Accounts = new ArrayList();
+       acc = new BankAccount(11121);
+       cust2Accounts.add(acc); //acc1ofcus2
+       acc = new BankAccount(11122);
+       cust2Accounts.add(acc); //acc2ofcus2
+       CustomerIf cust2 = new BankCustomer("Snow", cust2Accounts);
+      
+       
+       MyFacade obj = MyFacade.getMyFacade();
+       
+       obj.doDeposit(500.00, cust1, 11111);
+       obj.doDeposit(1500.00, cust1, 11112);
+       
+       obj.doDeposit(2400.00, cust2, 11121);
+       obj.doDeposit(6500.00, cust2, 11122);
+       
+       obj.getbankAccount(cust1, 11111);
+       obj.getbankAccount(cust1, 11112);
+       
+       obj.getbankAccount(cust2, 11121);
+       obj.getbankAccount(cust2, 11122);
+       
+       obj.getBanCustomer(cust1);
+       
     }
     
 }
